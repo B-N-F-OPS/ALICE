43af0a50c07bfc88c24d8b9d2234e94d88688eca
Brian <briankuria7@gmail.com>
2025-07-07 22:37:17 +0300

    update chatbot width

---
+++ index.js
@@ -0,0 +1,10 @@
+// when user clicks send activate this function to collect the input text.
+let inputTextEL = document.getElementById("inputText").value
+let openTextEL = document.getElementById("opentext")
+
+function send() {       // testing if i can put the input tect and display it in opentext
+    openTextEL.textContent = inputTextEL
+}
+
+// I need to display the input text in the chatbox, then pick it from there and use it in the next steps
+// explore tables
