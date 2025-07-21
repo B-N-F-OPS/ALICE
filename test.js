4b24d1f82f2c32a4d9e45004c53a0f065c87e189
Brian <briankuria7@gmail.com>
2025-07-21 19:08:31 +0300

    updated styling

--- test.js
+++
@@ -1,6 +0,0 @@
-function sendInput() {
-    let userQuizEL = document.getElementById("userQuiz")
-    let botAnsEL = document.getElementById("botAns")
-    let userMessageEL = document.getElementById("userMessage").value
-    userQuizEL.textContent = userMessageEL
-}
