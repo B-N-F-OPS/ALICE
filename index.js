e9c1b97068bcd72a35f2308c90bbf4a8fa421dc9
Brian <briankuria7@gmail.com>
2025-07-22 13:57:43 +0300

    sematic html update
    reducing div tags

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// when user clicks send activate this function to collect the input text.
-let inputTextEL = document.getElementById("inputText").value
-let openTextEL = document.getElementById("opentext")
+// I created IDs ("botMessage-el" && "clientMessage-el"). they have no css styling. i can delete if i want
 
-function send() {       // testing if i can put the input tect and display it in opentext
-    openTextEL.textContent = inputTextEL
-}
 
-// I need to display the input text in the chatbox, then pick it from there and use it in the next steps
-// explore tables. i might need to use <table> instead of <form> or use table inside <form>
-// add icon to the chats when bot is responding
